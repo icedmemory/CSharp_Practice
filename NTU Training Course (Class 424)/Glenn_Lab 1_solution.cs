c73d95bfb6b136ea2920008d6541759c76f158db
icedmemory <91enn63@gmail.com>
2024-11-09 16:52:58 +0800

    method(s): Random.Next( ), do-while (incl. break), int.Parse( ), if-else, if-elseif-else, if

---
+++ NTU Training Course (Class 424)/Glenn_Lab 1_solution.cs
@@ -0,0 +1,50 @@
+﻿namespace Glenn_Lab_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int min = 0, max = 99;
+            
+            Random rnd = new Random();
+            int secretNumber = rnd.Next(100);
+            // Console.WriteLine(secretNumber + "\n"); [see what the secret number is]
+            
+            int input, numbersExceptSecretNumber;
+            do
+            {
+                Console.WriteLine("({0}, {1})?", min, max);
+                input = int.Parse(Console.ReadLine());
+                if (input >= min && input <= max)
+                {
+                    if (input < secretNumber)
+                    {
+                        min = input + 1;
+                    }
+                    else if (input > secretNumber)
+                    {
+                        max = input - 1;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Bingo.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Out of range. Try again?");
+                }
+                
+                numbersExceptSecretNumber = max - min;
+                /* Console.WriteLine(numbersExceptSecretNumber); [check how many numbers are left,
+                except the secret number] */
+                if (numbersExceptSecretNumber == 0)
+                {
+                    Console.WriteLine("Game over.");
+                    break;
+                }
+            }
+            while (input != secretNumber);
+        }
+    }
+}
